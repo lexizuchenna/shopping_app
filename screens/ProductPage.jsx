8a29902da3162ae9303ed909d4c26c679cdb22db
Alexander Ukwueze <lextechspec@gmail.com>
2024-01-27 15:37:36 +0100

    Win 1

--- screens/ProductPage.jsx
+++ screens/ProductPage.jsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 const ProductPage = () => {
   const route = useRoute();
 
   const { product } = route.params;
-  console.log(product);
   return (
     <View>
       <Text>ProductPage</Text>
